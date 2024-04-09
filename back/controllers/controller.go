2a27fc4af0d0500a804c87812fb13380487ea975
Ilies <ilies.slim77@gmail.com>
2024-04-09 13:56:07 +0200

    Ajout du swagger et de son interface

--- back/controllers/controller.go
+++ back/controllers/controller.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"challenge/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// gin-swagger middleware
+// swagger embed files
 
 func ping(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -9,8 +17,17 @@ func ping(c *gin.Context) {
 }
 
 func RegisterRoutes(r *gin.Engine) {
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	api := r.Group("/")
 	{
+		api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 		api.GET("/ping", ping)
 		users := api.Group("/users")
 		{
