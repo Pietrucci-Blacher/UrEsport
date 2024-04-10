16df8838a009f2ee6ad8d29e322892872c32c356
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-04-10 13:59:25 +0200

    Feat: Edit App Name

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Esports App',
+      title: 'UrEsport',
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
       theme: ThemeData(
--- front/lib/screens/home_screen.dart
+++ front/lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Esports App'),
+        title: const Text('UrEsport'),
       ),
       body: const Center(
         child: Text('Welcome to the Home Screen!'),
