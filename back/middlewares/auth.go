b9a5741a4722fa7757eb494ccd9df7621024b376
teddy <teddy.gauthier@outlook.com>
2024-04-11 16:23:48 +0200

    feat(middleware) : add middleware in user and feature

--- back/middlewares/auth.go
+++ back/middlewares/auth.go
@@ -2,15 +2,17 @@ package middlewares
 
 import (
 	"challenge/models"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func IsLoggedIn() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user models.User
 		token, err := c.Cookie("auth_token")
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 			c.Abort()
 			return
 		}
@@ -29,21 +31,20 @@ func IsLoggedIn() gin.HandlerFunc {
 			return
 		}
 
-		c.Set("userId", userClaim.UserID)
+		if err := user.FindOneById(userClaim.UserID); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not Found"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user", user)
 		c.Next()
 	}
 }
 
 func IsAdmin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		userId, _ := c.MustGet("userId").(int)
-		var user models.User
-
-		if err := user.FindOneById(userId); err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-			c.Abort()
-			return
-		}
+		user, _ := c.MustGet("user").(models.User)
 
 		if !user.IsRole("admin") {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
