41acd3a95f033bd12a8d974913894bddf17fb186
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-04-19 14:18:34 +0200

    Feat: Add routing and navigation

---
+++ front/lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:uresport/shared/routing/routing.dart';
+import 'package:uresport/shared/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UrEsport',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+      home: const SplashScreen(),
+    );
+  }
+}
