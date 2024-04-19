a90bea28a4c28149aed7f9cb402a4ee98d3d4a79
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-04-19 14:18:34 +0200

    Feat: Add routing and navigation

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:uresport/screens/splash_screen.dart';
+import 'app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UrEsport',
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-    );
-  }
 }
---
+++ front/lib/tournament/screens/tournament_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TournamentScreen extends StatelessWidget {
+  const TournamentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Welcome to the Tournament Screen!'),
+    );
+  }
+}
