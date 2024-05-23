2a317e28b2b6337f1d06aee9a5bfc30921f05bb9
Ilies <ilies.slim77@gmail.com>
2024-05-23 21:31:17 +0200

    ajout page de chaque jeux

--- front/lib/widgets/games_screen.dart
+++ front/lib/widgets/games_screen.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'game_detail.dart';
-
-class Game {
-  final String name;
-  final String description;
-  final String imageUrl;
-
-  Game({
-    required this.name,
-    required this.description,
-    required this.imageUrl,
-  });
-}
+import 'package:uresport/models/game.dart';
 
 class GamesScreen extends StatelessWidget {
   final List<Game> games = generateGames();
