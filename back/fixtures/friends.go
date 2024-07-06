4876758d776616c2bcc7cfb60eece33327a7ec04
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-07-06 18:12:35 +0200

    Fix: Fix lint messages

--- back/fixtures/friends.go
+++ back/fixtures/friends.go
@@ -1,33 +1,30 @@
 package fixtures
 
 import (
-	"challenge/models"
-	"fmt"
-	"math/rand"
-	"time"
+    "challenge/models"
+    "fmt"
+    "math/rand"
+    "time"
 )
 
 func LoadFriends() error {
-	// Seed the random number generator
-	rand.Seed(time.Now().UnixNano())
+    rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for i := 0; i < FRIEND_NB; i++ {
-		// Generate random integers between 1 and 3
-		userID := rand.Intn(3) + 1
-		friendID := rand.Intn(3) + 1
+    for i := 0; i < FRIEND_NB; i++ {
+       userID := rng.Intn(3) + 1
+       friendID := rng.Intn(3) + 1
 
-		// Ensure friendID is not the same as userID
-		for friendID == userID {
-			friendID = rand.Intn(3) + 1
-		}
+       for friendID == userID {
+          friendID = rng.Intn(3) + 1
+       }
 
-		_, err := models.CreateFriend(userID, friendID, false)
-		if err != nil {
-			return err
-		}
+       _, err := models.CreateFriend(userID, friendID, false)
+       if err != nil {
+          return err
+       }
 
-		fmt.Printf("Friendship between user %d and user %d created\n", userID, friendID)
-	}
+       fmt.Printf("Friendship between user %d and user %d created\n", userID, friendID)
+    }
 
-	return nil
+    return nil
 }
