82a5d94ddfe98e3a71a0c60badf149a61067ab11
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-07-11 12:24:56 +0200

    Fix: Fix unhandled errors in default image

--- front/lib/shared/utils/image_util.dart
+++ front/lib/shared/utils/image_util.dart
@@ -1,8 +1,9 @@
 import 'dart:typed_data';
+
 import 'package:dio/dio.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:extended_image/extended_image.dart';
 
 class CachedImageWidget extends StatelessWidget {
   final String url;
@@ -12,77 +13,47 @@ class CachedImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExtendedImage.network(
-      url,
-      width: size,
-      height: size,
-      fit: BoxFit.cover,
-      shape: BoxShape.circle,
-      cache: true,
-      loadStateChanged: (state) {
-        switch (state.extendedImageLoadState) {
-          case LoadState.loading:
-            return const CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.grey,
-              child: CircularProgressIndicator(),
-            );
-          case LoadState.completed:
-            return ExtendedImage.network(
-              url,
-              width: size,
-              height: size,
-              fit: BoxFit.cover,
-              shape: BoxShape.circle,
-              cache: true,
-            );
-          case LoadState.failed:
-            return FutureBuilder<Widget>(
-              future: _buildSvgOrDefaultAvatar(url, size),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.grey,
-                  );
-                } else if (snapshot.hasError || !snapshot.hasData) {
-                  return const CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/default_avatar.png'),
-                  );
-                } else {
-                  return snapshot.data!;
-                }
-              },
-            );
-          default:
-            return const CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/default_avatar.png'),
-            );
+    return FutureBuilder<Widget>(
+      future: _loadImage(url, size),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.grey,
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError || !snapshot.hasData) {
+          return const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/default_avatar.png'),
+          );
+        } else {
+          return ClipOval(child: snapshot.data!);
         }
       },
     );
   }
 
-  Future<Widget> _buildSvgOrDefaultAvatar(String url, double size) async {
+  Future<Widget> _loadImage(String url, double size) async {
     try {
       final response = await Dio()
           .get(url, options: Options(responseType: ResponseType.bytes));
-
       final Uint8List bytes = Uint8List.fromList(response.data);
 
       if (isSvg(bytes)) {
+        final cleanedSvgBytes = cleanSvg(bytes);
         return SvgPicture.memory(
-          bytes,
+          cleanedSvgBytes,
           width: size,
           height: size,
           fit: BoxFit.cover,
         );
       } else {
-        return const CircleAvatar(
-          radius: 50,
-          backgroundImage: AssetImage('assets/default_avatar.png'),
+        return ExtendedImage.memory(
+          bytes,
+          width: size,
+          height: size,
+          fit: BoxFit.cover,
         );
       }
     } catch (e) {
@@ -96,5 +67,12 @@ class CachedImageWidget extends StatelessWidget {
   bool isSvg(Uint8List bytes) {
     const String svgHeader = '<svg';
     return String.fromCharCodes(bytes).trim().startsWith(svgHeader);
+  }
+
+  Uint8List cleanSvg(Uint8List bytes) {
+    String svgString = String.fromCharCodes(bytes);
+    svgString =
+        svgString.replaceAll(RegExp(r'<metadata[^>]*>.*?<\/metadata>'), '');
+    return Uint8List.fromList(svgString.codeUnits);
   }
 }
