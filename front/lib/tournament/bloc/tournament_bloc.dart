42f0446e0c372462e69f243b84a99bcc58f1eeeb
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-07-12 18:36:06 +0200

    Fix: Fix all shit in dart analysis

--- front/lib/tournament/bloc/tournament_bloc.dart
+++ front/lib/tournament/bloc/tournament_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uresport/core/services/tournament_service.dart';
 import 'package:uresport/tournament/bloc/tournament_event.dart';
@@ -14,10 +15,10 @@ class TournamentBloc extends Bloc<TournamentEvent, TournamentState> {
           limit: event.limit,
           page: event.page,
         );
-        print('Fetched ${tournaments.length} tournaments');
+        debugPrint('Fetched ${tournaments.length} tournaments');
         emit(TournamentLoadSuccess(tournaments: tournaments));
       } catch (e) {
-        print('Error loading tournaments: $e');
+        debugPrint('Error loading tournaments: $e');
         emit(TournamentLoadFailure());
       }
     });
