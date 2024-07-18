a5b005c6488a4639130f496656347c6c4af14d7a
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-07-18 11:30:28 +0200

    Fix: Dart Analyze issue

--- front/lib/bracket/screens/custom_bracket.dart
+++ front/lib/bracket/screens/custom_bracket.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tournament_bracket/tournament_bracket.dart';
